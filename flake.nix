c8a26a028410ea3b2b9e88d2485cf4f4d967b8d4
Neil Hooey <nhooey@gmail.com>
2025-07-27 02:00:41 +0700

    Added Nix with `gradle2nix` for the `shadowJar` task.
    Locked Gradle dependencies in `gradle.lock` with this command:
    
    ```
    nix develop
    nix run github:tadfisher/gradle2nix/v2#gradle2nix -- --task shadowJar
    ```

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "XTDB - the temporal database";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gradle2nix = {
+      url = "github:tadfisher/gradle2nix/v2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, gradle2nix }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages = {
+        default = gradle2nix.builders.${system}.buildGradlePackage {
+          pname = "xtdb";
+          version = "2.x-SNAPSHOT";
+          src = self;
+          sourceRoot = "source";
+          lockFile = ./gradle.lock;
+          gradleFlags = [ "-Pversion=2.x-SNAPSHOT" ];
+          gradleInstallFlags = [ "build" "shadowJar" ];
+          java = nixpkgs.legacyPackages.${system}.jdk21;
+          nativeBuildInputs = with nixpkgs.legacyPackages.${system}; [
+            gradle
+          ];
+          doCheck = false;
+          meta = {
+            description = "XTDB - the temporal database";
+            homepage = "https://xtdb.com";
+            license = nixpkgs.lib.licenses.mpl20;
+            mainProgram = "xtdb";
+          };
+        };
+      };
+
+      devShells.default = nixpkgs.legacyPackages.${system}.mkShell {
+        buildInputs = with nixpkgs.legacyPackages.${system}; [
+          jdk21
+          gradle
+        ] ++ (if (gradle2nix.packages ? ${system} && gradle2nix.packages.${system} ? default)
+        then [ gradle2nix.packages.${system}.default ]
+        else [ ]);
+      };
+    });
+}
